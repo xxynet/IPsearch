97c8a4bbbb69b37f579f52b70a8e9d0eaf40e742
xxynet <xxy3429924750@126.com>
2021-07-01 20:38:14 +0800

    1

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-function GetIPInfo(ip)
+function SearchIP(ip)
 {
-    $.getJSON("https://api.xsot.cn/ip/?ip="+ip,function(data,status)
-        {
-           return data.addr;
-        })
+    $.getJSON("https://api.xsot.cn/ip/?ip="+ip,function(data,status){
+        return data.addr;
+    })
 }
+
+
+
+{/* <script>
+    $(document).ready(function(){
+        $("#search").click(function(){
+            // $("#sip").text(GetIPInfo(ip))
+                $("#sip").text("查询中……")
+                $("#cip").text("");
+                var ip = $("#ip").val();
+                $.getJSON("https://api.xsot.cn/ip/?ip="+ip,function(data,status){
+                    $("#sip").text(ip)
+                    $("#cip").text(data.addr);
+                })
+            })
+    })
+</script> */}
